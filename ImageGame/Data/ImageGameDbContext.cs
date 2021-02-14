9ced06fa8e48141357b91af2d03a24a4a5accd39
AhmedAlzubaidi <codeLimitless@gmail.com>
2021-02-14 05:03:28 +0300

    Completed DbContext

--- ImageGame/Data/ImageGameDbContext.cs
+++ ImageGame/Data/ImageGameDbContext.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using ImageGame.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ImageGame.Data
 {
-    public class ImageGameDbContext
+    public class ImageGameDbContext : DbContext
     {
+        public ImageGameDbContext(DbContextOptions<ImageGameDbContext> options) : base(options) {}
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Image> Images { get; set; }
     }
 }
