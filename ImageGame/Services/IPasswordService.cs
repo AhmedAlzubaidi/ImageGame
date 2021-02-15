2799744a2220aaed708cfc88b4a2a76b9168c36c
AhmedAlzubaidi <codeLimitless@gmail.com>
2021-02-15 06:07:25 +0300

    Added PasswordService

---
+++ ImageGame/Services/IPasswordService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageGame.Services
+{
+    interface IPasswordService
+    {
+        public string hash(string password);
+    }
+}
---
+++ ImageGame/Services/PasswordService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace ImageGame.Services
+{
+    public class PasswordService : IPasswordService
+    {
+        public string hash(string password)
+        {
+            // Generate salt
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+
+            // Generate hash
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+            return hashed;
+        }
+    }
+}
